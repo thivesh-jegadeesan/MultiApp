b7da926ca36c262dc13ebdfc99fefc372633fd84
thivesh-jegadeesan <151930863+thivesh-jegadeesan@users.noreply.github.com>
2024-02-13 15:16:57 +0000

    update completed

--- todo/urls.py
+++ todo/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path("home/", home,  name="home"),
     path("create/", create,  name="create"),
     path('create-new-todo/', create_new, name='create_new'),
+    path('edit/<int:pk>/', edit_todo, name='edit_todo'),
+    path('update_task-todo/<int:pk>/', update_task, name='update_task'),
+
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import TodoModelForm
 from .models import Todo
 from django.urls import reverse
@@ -23,3 +23,23 @@ def create_new(request):
     else:
         form = TodoModelForm()
     return render(request, 'create.html', {'form': form})
+
+def edit_todo(request, pk):
+    record = get_object_or_404(Todo, pk=pk)
+    form = TodoModelForm(instance=record)  # Pass the instance to the form
+    return render(request, 'edit.html', {'form':form})
+
+def update_task(request, pk):
+    record = get_object_or_404(Todo, pk=pk)
+    if request.method == 'POST':
+        form = TodoModelForm(request.POST, instance=record)
+        if form.is_valid():
+            form.save()
+            # return redirect('todo/home/')
+            return redirect('home')
+
+    else:
+        form = TodoModelForm()
+    return render(request, 'edit.html', {'form': form})
+
+    
